8f7dbc1a313a147a40197b8b16a37f9e2771e903
Bhavik Patel <bhavik.patel@rallyhealth.com>
2020-11-14 10:12:53 -0600

    updated api

--- ktor-todo/todolist-restapi/src/main/kotlin/TodoApi.kt
+++ ktor-todo/todolist-restapi/src/main/kotlin/TodoApi.kt
@@ -1,14 +1,31 @@
 package todo
 
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 
 fun Routing.todoApi() {
-    route("/api/todo") {
-        get("/") {
+    route("/api") {
+        get("/todos") {
             call.respond(todos)
+        }
+
+        get("/todo/{id}") {
+            val id = call.parameters["id"]
+            id?.let {
+                try {
+                    val todo = todos[id.toInt()]
+                    call.respond(todo)
+                } catch (e: Throwable){
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+
+        post("/todos") {
+            call.respond(HttpStatusCode.Unauthorized)
         }
     }
 }
