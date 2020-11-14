b4ffc64350906af7cb2b1ad3fd541bb92bb400c8
Bhavik Patel <bhavik.patel@rallyhealth.com>
2020-11-14 09:22:17 -0600

    updated with mocked data

--- ktor-todo/todolist-restapi/src/main/kotlin/Application.kt
+++ ktor-todo/todolist-restapi/src/main/kotlin/Application.kt
@@ -1,26 +1,37 @@
 package todo
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.content.*
+import io.ktor.features.*
 import io.ktor.http.content.*
+import io.ktor.jackson.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
+
+val todos = listOf(todo1, todo2)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
+    install(Routing) {
+        todoApi()
+    }
 
-        // Static feature. Try to access `/static/ktor_logo.svg`
-        static("/static") {
-            resources("static")
+    install(StatusPages) {
+        this.exception<Throwable> { e ->
+            call.respondText(e.localizedMessage, ContentType.Text.Plain)
+            throw e
+        }
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
         }
     }
 }
---
+++ ktor-todo/todolist-restapi/src/main/kotlin/TodoApi.kt
@@ -0,0 +1,13 @@
+package todo
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Routing.todoApi() {
+    route("/api/todo") {
+        get("/") {
+            call.respond(todos)
+        }
+    }
+}
