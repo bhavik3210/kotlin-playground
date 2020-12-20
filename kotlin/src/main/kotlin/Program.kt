e3dfb5143ac9d6e233941f039dba66a996682e72
bhavik <bhavik@email.com>
2020-12-20 16:59:27 -0600

    removed windows gradle, updated project name

--- kotlin/src/main/kotlin/Program.kt
+++ kotlin/src/main/kotlin/Program.kt
@@ -4,16 +4,6 @@ import kotlin.math.floor
 
 fun main() {
 
-//    val decimalValue = 12.4
-//    val wholeValue = 12.0
-//
-//
-//    println("${decimalValue - floor(decimalValue)} : ${isWholeNumber(decimalValue)}")
-//    println("${wholeValue - floor(wholeValue)} : ${isWholeNumber(wholeValue)}")
-//
-//    println()
-//    println(ceil(decimalValue))
-//    println(ceil(wholeValue))
 
 
 
