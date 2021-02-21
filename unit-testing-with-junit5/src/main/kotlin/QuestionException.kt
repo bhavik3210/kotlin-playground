e198583716bed9d6de6d3884c00e28b94755bc5f
bhavik <bhavik@email.com>
2021-02-21 10:52:11 -0600

    deleted moved project

--- unit-testing-with-junit5/src/main/kotlin/QuestionException.kt
+++
@@ -1,2 +0,0 @@
-class QuestionException(message: String) : Throwable(message)
-class ServiceException(message: String) : Throwable(message)
--- unit-testing-with-junit5/src/test/kotlin/Annotations.kt
+++
@@ -1,6 +0,0 @@
-import org.junit.jupiter.api.TestInstance
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class KotlinParameterizedTests {}
